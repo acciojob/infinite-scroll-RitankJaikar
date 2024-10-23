90623b472cbc50f8c386611448bca9bb361ff7c3
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 16:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+const infiList= document.querySelector("#infi-list");
+
+let itemCount = 1;
+
+function addItems(num) {
+	for(let i=1; i<=num; i++) {
+		const li= document.createElement("li");
+		li.innerText= `Item ${itemCount++}`;
+		infiList.append(li);
+	}
+}
+
+addItems(10);
+
+infiList.addEventListener("scroll", () => {
+	//infiList.scrollTop- this value will be 0.
+	//infiList.clientHeight- represents the height of the visible area of the scrollable element
+	if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+	    addItems(10);
+	}
+})
+
+
+
 
